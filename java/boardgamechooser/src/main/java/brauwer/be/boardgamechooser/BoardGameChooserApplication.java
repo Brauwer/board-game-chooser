30cf2859f7344119a91fddfcdf37718c0893626e
Brauwer <thomasdebrauwer96@gmail.com>
2022-04-08 10:17:27 +0200

    Project initisialition

---
+++ java/boardgamechooser/src/main/java/brauwer/be/boardgamechooser/BoardGameChooserApplication.java
@@ -0,0 +1,13 @@
+package brauwer.be.boardgamechooser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameChooserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameChooserApplication.class, args);
+	}
+
+}
---
+++ java/boardgamechooser/src/test/java/brauwer/be/boardgamechooser/BoardGameChooserApplicationTests.java
@@ -0,0 +1,13 @@
+package brauwer.be.boardgamechooser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameChooserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
